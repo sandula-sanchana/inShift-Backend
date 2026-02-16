e9d8933b929b44aa9598420757c113b4d46c52b5
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-16 22:21:06 +0800

    add WebConfig

--- src/main/java/edu/ijse/inshiftbackend/controllers/GeoController.java
+++ src/main/java/edu/ijse/inshiftbackend/controllers/GeoController.java
@@ -1,4 +1,20 @@
 package edu.ijse.inshiftbackend.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("api/v1/geocode")
+@CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
 public class GeoController {
+
+    private final RestTemplate restTemplate;
+
+
+
+
 }
