1ef8bc34790ffcfdb875eff887e09671cc85aa32
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-16 13:28:19 +0800

    first commit

---
+++ src/main/java/edu/ijse/inshiftbackend/InshiftBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.ijse.inshiftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InshiftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InshiftBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ijse/inshiftbackend/InshiftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ijse.inshiftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InshiftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
