084c539f28c8ee98369c78ed757a8bdd45f70600
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-24 09:21:32 +0530

    add security dependency

--- src/main/java/edu/ijse/inshiftbackend/config/WebConfig.java
+++ src/main/java/edu/ijse/inshiftbackend/config/WebConfig.java
@@ -3,6 +3,8 @@ package edu.ijse.inshiftbackend.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -17,5 +19,10 @@ public class WebConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
