1646de1a88af7d5db533d622ef3903d54def9898
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-27 14:59:31 +0530

    add spring security dependency and register suerdetailservice bean

--- src/main/java/edu/ijse/inshiftbackend/config/WebConfig.java
+++ src/main/java/edu/ijse/inshiftbackend/config/WebConfig.java
@@ -1,15 +1,26 @@
 package edu.ijse.inshiftbackend.config;
 
+
+import edu.ijse.inshiftbackend.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig {
+
+    private final EmployeeRepository employeeRepository;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -24,5 +35,15 @@ public class WebConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email ->employeeRepository.findByEmail(email).map(
+                employee->org.springframework.security.core.userdetails.User.builder()
+                        .username(employee.getEmail())
+                        .password(employee.getPasswordHash())
+                        .authorities(List.of(new SimpleGrantedAuthority("ROLE_"+employee.getRole()))).build()
+        ).orElseThrow(()->new UsernameNotFoundException("User not found"));
     }
 }
