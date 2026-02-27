47ddb263454766b5870f1d6b9265c86058612c2a
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-27 17:59:19 +0530

    add AuthController

---
+++ src/main/java/edu/ijse/inshiftbackend/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package edu.ijse.inshiftbackend.controllers;
+
+import edu.ijse.inshiftbackend.dto.AuthDTO;
+import edu.ijse.inshiftbackend.dto.AuthResponseDTO;
+import edu.ijse.inshiftbackend.service.AuthService;
+import edu.ijse.inshiftbackend.util.APIResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public APIResponse<AuthResponseDTO> login(@RequestBody @Valid AuthDTO authDTO) {
+
+        AuthResponseDTO response = authService.login(authDTO);
+
+        return new APIResponse<>(
+                HttpStatus.OK.value(),
+                "Login Successful",
+                 response
+        );
+    }
+}
