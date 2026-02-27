033fa4afa6931e3328c8f2a670f676a006233b7a
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-27 18:44:10 +0530

    add authDTO validations

--- src/main/java/edu/ijse/inshiftbackend/dto/AuthDTO.java
+++ src/main/java/edu/ijse/inshiftbackend/dto/AuthDTO.java
@@ -1,5 +1,7 @@
 package edu.ijse.inshiftbackend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +10,10 @@ import lombok.Data;
 @Data
 @Builder
 public class AuthDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
