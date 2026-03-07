11be31d52b41ba8e593948987748e0677dcc8193
sandula-sanchana <sandulasanchana@gmail.com>
2026-03-07 20:37:00 +0530

    add passkey register verify dto

---
+++ src/main/java/edu/ijse/inshiftbackend/dto/PasskeyRegisterVerifyDTO.java
@@ -0,0 +1,27 @@
+package edu.ijse.inshiftbackend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasskeyRegisterVerifyDTO {
+
+    @NotBlank(message = "Credential ID is required")
+    private String credentialId;
+
+    @NotBlank(message = "Public key is required")
+    private String publicKey;
+
+    private Long signCount;
+
+    private String transports;
+
+    private String aaguid;
+
+    private String deviceName;
+}
--- src/main/java/edu/ijse/inshiftbackend/service/PasskeyService.java
+++ src/main/java/edu/ijse/inshiftbackend/service/PasskeyService.java
@@ -1,9 +1,12 @@
 package edu.ijse.inshiftbackend.service;
 
+import edu.ijse.inshiftbackend.dto.PasskeyRegisterVerifyDTO;
 import edu.ijse.inshiftbackend.dto.response.PasskeyRegisterOptionsDTO;
 import edu.ijse.inshiftbackend.entity.enums.WebAuthnChallengePurpose;
 
 public interface PasskeyService {
 
     PasskeyRegisterOptionsDTO getPasskeyRegisterResponse(WebAuthnChallengePurpose purpose);
+
+    void verifyAndSavePasskey(PasskeyRegisterVerifyDTO dto);
 }
